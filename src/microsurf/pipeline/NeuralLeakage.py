added optiomn to save traces to disk, fixed normalization, LSB_MSB issues

src/microsurf/pipeline/NeuralLeakage.py
@@ -26,7 +26,7 @@ class shiftedRELU(nn.Module):
 class MIEstimator(nn.Module):
     def __init__(self, X) -> None:
         super().__init__()
-        self.X = torch.tensor(minmax_scale(np.array(X, dtype=np.float32), (-1, 1)))
+        self.X = X
         self.T = nn.Sequential(
             nn.Linear(self.X.shape[1] * 2, 100),
             nn.ReLU(),
@@ -48,26 +48,32 @@ class MIEstimator(nn.Module):
 class NeuralLeakageModel(nn.Module):
     def __init__(self, X, Y, keylen, leakAddr, assetDir) -> None:
         super().__init__()
+        X = X.T[:1].T
+        self.X = X
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
-        self.X = minmax_scale(np.array(X, dtype=np.float64), (-1, 1))
+        #self.X = np.array(X, dtype=np.float32)
+        self.X = (X-X.mean()) / (X.std()+1e-5)
         self.keylen = keylen
         self.Y = self.binary(Y).reshape(Y.shape[0], keylen)
         self.OriginalY = Y
         self.assetDir = assetDir
-        self.HUnits = 2 * keylen
+        self.HUnits = 25
         self.LeakageModel = nn.Sequential(
             nn.Linear(keylen, self.HUnits),
+            nn.Dropout(0.5),
             nn.ReLU(),
-            nn.Dropout(0.3),
             nn.Linear(self.HUnits, self.HUnits),
+            nn.Dropout(0.5),
             nn.ReLU(),
             nn.Linear(self.HUnits, 1),
+            nn.ReLU(),
         ).to(self.device)
         self.leakAddr = leakAddr
         log.debug(f"{X.shape[0]} samples")
+        log.debug(f"{X.shape[1]} entries / samples")
 
     def train(self):
-        optim = torch.optim.Adam(self.LeakageModel.parameters(), lr=1e-4)
+        optim = torch.optim.Adam(self.LeakageModel.parameters(), lr=1e-3)
         mest = MIEstimator(self.X)
         X = []
         Y = []
@@ -75,7 +81,8 @@ class NeuralLeakageModel(nn.Module):
         l30 = 0
         l30avg = []
         earlyStop = False
-        for e in range(1,100):
+        zcount = 0
+        for e in range(1,200):
             lpred = self.LeakageModel(self.Y)
             mest.trainEstimator(lpred)
             loss = -mest.forward(lpred)
@@ -83,8 +90,10 @@ class NeuralLeakageModel(nn.Module):
             loss.backward()
             l30avg.append(loss.cpu().item())
             # stop learning if MI doesn't increase => FP
-            if e >= 100 and -loss < 0.0001:
-                break
+            if -loss < 0.0001:
+                zcount += 1
+                if zcount > 30:
+                    break
             optim.step()
             # good enough, stop
             if -loss > 1.0:
@@ -109,12 +118,11 @@ class NeuralLeakageModel(nn.Module):
         mest.trainEstimator(lpred)
         self.MIScore = mest.forward(lpred)
 
-        import matplotlib.pyplot as plt
-        fig, ax = plt.subplots()
-        ax.plot(X,Y)
-        fig.savefig(f"loss{hex(self.leakAddr)}.png")
         if self.MIScore > 0.1:
-
+            import matplotlib.pyplot as plt
+            fig, ax = plt.subplots()
+            ax.plot(X,Y)
+            fig.savefig(f"loss{hex(self.leakAddr)}.png")
             input = torch.ones((1, self.keylen)).to(self.device)
             self.LeakageModel.eval()
             input.requires_grad = True
@@ -126,8 +134,9 @@ class NeuralLeakageModel(nn.Module):
             plt.tight_layout()
             f, (ax, cbar_ax) = plt.subplots(2, gridspec_kw=grid_kws, figsize=(7, 2))
             ax = sns.heatmap(
-                keys[:, None].T,
+                keys[::-1, None].T,
                 ax=ax,
+                xticklabels=[(self.keylen - i) for i in range(self.keylen) if i % 2 == 0], # MSB to LSB
                 cbar_ax=cbar_ax,
                 cbar_kws={"orientation": "horizontal"},
                 square=True,
@@ -146,7 +155,7 @@ class NeuralLeakageModel(nn.Module):
     def binary(self, Y):
         YL = np.zeros((Y.shape[0], self.keylen))
         for i,x in enumerate(Y): # row
-            binR = bin(x[0].item())[2:].zfill(self.keylen)
+            binR = bin(x[0].item())[2:].zfill(self.keylen)[::-1]
             for j,bit in enumerate(binR): # col
                 YL[i][j] = int(bit)
-        return  torch.tensor(YL).to(self.device, dtype=torch.float32)
+        return  torch.tensor(YL, dtype=torch.float32).to(self.device, dtype=torch.float32)