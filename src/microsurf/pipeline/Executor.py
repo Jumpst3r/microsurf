8562531b5d90ac8cd9ac0cf7fe1faec2897bbe5a
Jumpst3r <nicolas.dutly@pm.me>
2022-04-07 16:24:55 +0200

    increased trace count

--- src/microsurf/pipeline/Executor.py
+++ src/microsurf/pipeline/Executor.py
@@ -23,7 +23,7 @@ class PipeLineExecutor:
     def __init__(self, loader: BinaryLoader) -> None:
         self.loader = loader
         self.results: List[int] = []
-        self.ITER_COUNT = 100
+        self.ITER_COUNT = 500
         self.multiprocessing = True
 
     def run(self, detector):
